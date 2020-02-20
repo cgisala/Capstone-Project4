bfe0a1d947a0d30e684216c39f9c387f4fb7b8b9
chris <chris@chris-pc>
2020-02-19 20:49:05 -0600

    First commit of project 4

---
+++ hotel.py
@@ -0,0 +1,7 @@
+import sqlite3
+import os
+
+db = os.path.join('database', 'travel_app.db')
+
+class Hotel():
+    pass
---
+++ news.py
@@ -0,0 +1,7 @@
+import sqlite3
+import os
+
+db = os.path.join('database', 'travel_app.db')
+
+class News():
+    pass
